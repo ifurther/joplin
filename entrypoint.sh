f8c70220149be7371a13c7187c7d9decc81a380d
ifurther <55025025+ifurther@users.noreply.github.com>
2022-02-02 22:35:03 +0800

    add for using volume for user
    set CMD
    
    add reading env uid and gid for entrypoint
    
    using current_id
    
    inverse if loop:
    
    update usermod and groupmod;
    
    mkdir server log

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,5 @@
-#!/bin/sh
+#!/usr/bin/env bash
+set -Eeo pipefail
 
 isCommand() {
   
@@ -8,18 +9,40 @@ isCommand() {
     return 1 
   fi
 }
+if [[ ! -z "$UID" ]]; then
+  Current_id==$UID
+  if [[ -z "$GID" ]]; then
+    Current_group_id=$GID
+  else
+    Current_group_id=$UID
+  fi
+else
+  Current_id=$(id -u)
+  Current_group_id=$(id -g)
+fi
 
-Current_id=$(id -u)
-Current_group_id=$(id -g)
 APP_id=$(id -u USER)
 APP_group_id=$(id -g USER)
 
-if [ $Current_id != $APP_id ]; then
-  groupmod --gid $Current_group_id USER
-  usermod --uid $Current_id USER
+if [ $Current_id != 0 ] && [ $Current_id != $APP_id ]; then
+  groupmod -o -g $Current_group_id USER
+  usermod -o -u $Current_id USER
 fi
-if isCommand "$1"; then
-  set -- /usr/sbin/gosu USER:USER /usr/bin/tini -- node dist/app.js "$@"
+if [[ ! -z "$Path" ]] ; then
+  mkdir -p $Path || :
+  # allow the container to be started with `--user`
+  if [ "$user" = '0' ]; then
+    find "$Path" \! -user USER -exec chown USER '{}' +
+  fi
+fi
+
+exec /usr/bin/gosu USER:USER mkdir -f /home/USER/packages/server/logs
+
+if [ "$Current_id" = "0" ]; then
+  echo "Switching to dedicated user 'USER'"
+  if isCommand "$1"; then
+    set -- /usr/sbin/gosu USER:USER /usr/bin/tini -- "$@"
+  fi
 fi
 
 exec "$@"
