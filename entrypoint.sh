fb8f9d2f5631e3f402235a6532618431376ccdb2
ifurther <55025025+ifurther@users.noreply.github.com>
2022-02-04 15:28:40 +0800

    change to use volume
    user mode fix
    
    fix error
    
     update entrypoint
    
    update entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,55 +1,14 @@
 #!/usr/bin/env bash
 set -Eeo pipefail
 
-isCommand() {
-  
-  if [ "$1" = "sh" ]; then
-    return 1
-  elif [ "$1" = "bash" ]; then
-    return 1 
-  fi
-}
-if [[ ! -z "$PUID" ]]; then
-  Current_id=$PUID
-  if [[ ! -z "$PGID" ]]; then
-    Current_group_id=$PGID
-  else
-    Current_group_id=$PUID
-  fi
-else
-  Current_id=$(id -u)
-  Current_group_id=$(id -g)
+if [[ "$*" == node*dist/app.js* ]] && [ "$(id -u)" = '0' ]; then
+	find $Path \! -user USER -exec chown USER '{}' +
+	exec /usr/bin/tini -- /usr/sbin/gosu USER:USER "$@"
 fi
 
-APP_id=$(id -u USER)
-APP_group_id=$(id -g USER)
-
-if [ $Current_id != $APP_id ]; then
-  groupmod -o -g "$Current_group_id" USER || return 1
-  usermod -o -u "$Current_id" USER || return 1
+if [[ "$*" == node*dist/app.js* ]]; then
+	mkdir -p /home/USER/packages/server/logs || return 1
+	set -- /usr/bin/tini -- "$@"
 fi
 
-echo "
-User uid:    $(id -u USER)
-User gid:    $(id -g USER)
--------------------------------------
-"
-
-if [[ ! -z "$Path" ]] ; then
-  mkdir -p $Path || :
-  # allow the container to be started with `--user`
-  if [ "$user" = '0' ]; then
-    find "$Path" \! -user USER -exec chown USER '{}' +
-  fi
-fi
-
-exec /usr/sbin/gosu USER:USER mkdir -f /home/USER/packages/server/logs
-
-if [ "$Current_id" = "0" ]; then
-  echo "Switching to dedicated user 'USER'"
-  if isCommand "$1"; then
-    set -- /usr/bin/tini -- /usr/sbin/gosu USER:USER "$@"
-  fi
-fi
-
-exec /usr/bin/tini -- "$@"
+exec "$@"
